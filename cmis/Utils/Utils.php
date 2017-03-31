c800ee8d6008103e45b6b7be980add63c24be9ee
Alexis DUCERF <alexis.ducerf@gmail.com>
2017-03-31 16:19:49 +0200

    EOL for logging

--- cmis/Utils/Utils.php
+++ cmis/Utils/Utils.php
@@ -23,9 +23,9 @@ class Utils
     {
         file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', $_SERVER['REQUEST_METHOD'] . ' ' . http_response_code() . ' - ' . $_SERVER['REQUEST_URI'] . ' - [' . date('d/m/Y H:i:s') . ']' . PHP_EOL, FILE_APPEND);
         if (!empty($_GET)) file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', 'GET : ' . print_r($_GET, true) . PHP_EOL, FILE_APPEND);
-        if (!empty($_POST)) file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', 'POST : ' . print_r($_POST, true), FILE_APPEND);
-        if (!empty($_FILES)) file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', 'FILES : ' . print_r($_FILES, true), FILE_APPEND);
-        file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', 'RAW : ' . print_r(file_get_contents('php://input'), true), FILE_APPEND);
+        if (!empty($_POST)) file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', 'POST : ' . print_r($_POST, true). PHP_EOL, FILE_APPEND);
+        if (!empty($_FILES)) file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', 'FILES : ' . print_r($_FILES, true). PHP_EOL, FILE_APPEND);
+        file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', 'RAW : ' . print_r(file_get_contents('php://input'), true). PHP_EOL, FILE_APPEND);
         file_put_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR .'logs/access.log', ' --------------------- ' . PHP_EOL, FILE_APPEND);
     }
 
