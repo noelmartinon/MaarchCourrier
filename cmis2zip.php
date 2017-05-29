274a216151e4b8cfa7ff459f69675a5c77d2d917
Alexis DUCERF <alexis.ducerf@gmail.com>
2017-05-29 16:55:34 +0200

    Export CMIS into a ZIP file

---
+++ cmis2zip.php
@@ -0,0 +1,55 @@
+<?php
+
+$rootPath = realpath('cmis');
+
+$zip = new ZipArchive();
+$zip->open('cmis.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+$zip->addEmptyDir('cmis');
+foreach ($files as $name => $file)
+{
+    if (!$file->isDir())
+    {
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        if ($file->getFilename() != 'access.log')
+        {
+            $zip->addFile($filePath, 'cmis/' .  $relativePath);
+        }
+
+    }
+}
+
+$zip->addEmptyDir('cmis/logs');
+
+$rootPath = realpath('modules/folder/Models');
+
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+
+$zip->addEmptyDir('modules/folder/Models');
+
+foreach ($files as $name => $file)
+{
+    if (!$file->isDir())
+    {
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        $zip->addFile($filePath, 'modules/folder/Models/' . $relativePath);
+    }
+}
+
+$zip->close();
+header("Location: cmis.zip");
