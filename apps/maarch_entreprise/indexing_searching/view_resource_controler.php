4d78abbbcac860f6a1045c37a2435cfb96d55ae5
florian.azizian <florian.azizian@maarch.org>
2017-11-08 12:26:19 +0100

    FEAT #6118 attachment preview was display in attachment tab and document tab

--- apps/maarch_entreprise/indexing_searching/view_resource_controler.php
+++ apps/maarch_entreprise/indexing_searching/view_resource_controler.php
@@ -117,39 +117,53 @@ if ($s_id == '') {
         $res_outgoing = $stmt->fetchObject(); 
 
         if ($res_outgoing->category_id == 'outgoing' && $res_outgoing->source == 'with_empty_file') {
-            $stmt = $db->query("SELECT res_id FROM res_view_attachments WHERE status <> 'DEL' and status <> 'OBS' "
-                . "and res_id_master = ? and coll_id = ? and ((attachment_type = 'converted_pdf' and type_id = 1) "
-                . "OR (attachment_type = 'outgoing_mail' and format = 'pdf')"
-                . "OR (attachment_type = 'signed_response' and format = 'pdf')) order by res_id desc", 
-                array($s_id, $_SESSION['collection_id_choice']));
-            $res_att = $stmt->fetchObject();
-            if ($stmt->rowCount() > 0) {
-                ?>
-                <script type="text/javascript">
-                window.location.href = '<?php
-                    echo $_SESSION['config']['businessappurl'];
-                    ?>index.php?display=true&module=attachments&page=view_attachment&res_id_master=<?php echo $s_id;?>&id=<?php echo $res_att->res_id;?>'
-                </script>
-                <?php
-                exit();
-            }  else {
-            	$stmt = $db->query("SELECT res_id FROM res_view_attachments WHERE status <> 'DEL' and status <> 'OBS' "
-                    . "and res_id_master = ? and coll_id = ? and ((attachment_type = 'converted_pdf' and (type_id = 1 or type_id = 0)) "
-                    . "OR (attachment_type = 'outgoing_mail' and format = 'pdf')"
-                    . "OR (attachment_type = 'signed_response' and format = 'pdf')) order by res_id desc",
+            if(!empty($_REQUEST['editingMode'])){
+                $stmt = $db->query("SELECT res_id FROM res_view_attachments WHERE status <> 'DEL' and status <> 'OBS' "
+                    . "and res_id_master = ? and coll_id = ? "
+                    . "and attachment_type = 'outgoing_mail' order by res_id desc", 
                     array($s_id, $_SESSION['collection_id_choice']));
-            	$res_att = $stmt->fetchObject();
-            	if ($stmt->rowCount() > 0) {
-	            ?>
-	            <script type="text/javascript">
-	                window.location.href = '<?php
-	                echo $_SESSION['config']['businessappurl'];
-	                ?>index.php?display=true&module=attachments&page=view_attachment&res_id_master=<?php 
-                        echo $s_id;?>&id=<?php echo $res_att->res_id;?>'
-	            </script>
-	            <?php
-	            exit();
-	        }
+                $res_att = $stmt->fetchObject();
+                if ($stmt->rowCount() > 0) { ?>
+                    <script type="text/javascript">
+                    window.location.href = 'index.php?display=true&editingMode=true&module=attachments&page=view_attachment&res_id_master=<?php echo $s_id;?>&id=<?php echo $res_att->res_id;?>'
+                    </script>
+                    <?php exit();
+                }
+            } else {
+                $stmt = $db->query("SELECT res_id FROM res_view_attachments WHERE status <> 'DEL' and status <> 'OBS' "
+                    . "and res_id_master = ? and coll_id = ? and ((attachment_type = 'converted_pdf' and type_id = 1) "
+                    . "OR (attachment_type = 'outgoing_mail' and format = 'pdf')"
+                    . "OR (attachment_type = 'signed_response' and format = 'pdf')) order by res_id desc", 
+                    array($s_id, $_SESSION['collection_id_choice']));
+                $res_att = $stmt->fetchObject();
+                if ($stmt->rowCount() > 0) {
+                    ?>
+                    <script type="text/javascript">
+                    window.location.href = '<?php
+                        echo $_SESSION['config']['businessappurl'];
+                        ?>index.php?display=true&module=attachments&page=view_attachment&res_id_master=<?php echo $s_id;?>&id=<?php echo $res_att->res_id;?>'
+                    </script>
+                    <?php
+                    exit();
+                }  else {
+                	$stmt = $db->query("SELECT res_id FROM res_view_attachments WHERE status <> 'DEL' and status <> 'OBS' "
+                        . "and res_id_master = ? and coll_id = ? and ((attachment_type = 'converted_pdf' and (type_id = 1 or type_id = 0)) "
+                        . "OR (attachment_type = 'outgoing_mail' and format = 'pdf')"
+                        . "OR (attachment_type = 'signed_response' and format = 'pdf')) order by res_id desc",
+                        array($s_id, $_SESSION['collection_id_choice']));
+                	$res_att = $stmt->fetchObject();
+                	if ($stmt->rowCount() > 0) {
+        	            ?>
+        	            <script type="text/javascript">
+        	                window.location.href = '<?php
+        	                echo $_SESSION['config']['businessappurl'];
+        	                ?>index.php?display=true&module=attachments&page=view_attachment&res_id_master=<?php 
+                                echo $s_id;?>&id=<?php echo $res_att->res_id;?>'
+        	            </script>
+        	            <?php
+        	            exit();
+        	        }
+                }
             }
         }
     }
