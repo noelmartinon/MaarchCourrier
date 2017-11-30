2786ade931581a94167d3c86bf85efa7d688d885
Damien <damien.burel@maarch.org>
2017-11-30 14:49:41 +0100

    FEAT Ozwillo config

--- core/Models/CoreConfigModel.php
+++ core/Models/CoreConfigModel.php
@@ -126,9 +126,13 @@ class CoreConfigModel
         return $loggingMethod;
     }
 
-    public static function getOzwilloConfiguration()
+    public static function getOzwilloConfiguration(array $aArgs = [])
     {
-        $customId = CoreConfigModel::getCustomId();
+        if (empty($aArgs['customId'])) {
+            $customId = CoreConfigModel::getCustomId();
+        } else {
+            $customId = $aArgs['customId'];
+        }
 
         if (file_exists("custom/{$customId}/apps/maarch_entreprise/xml/ozwilloConfig.xml")) {
             $path = "custom/{$customId}/apps/maarch_entreprise/xml/ozwilloConfig.xml";
@@ -140,9 +144,11 @@ class CoreConfigModel
         if (file_exists($path)) {
             $loadedXml = simplexml_load_file($path);
             if ($loadedXml) {
-                $ozwilloConfig['uri']           = (string)$loadedXml->URI;
-                $ozwilloConfig['clientId']      = (string)$loadedXml->CLIENT_ID;
-                $ozwilloConfig['clientSecret']  = (string)$loadedXml->CLIENT_SECRET;
+                $ozwilloConfig['instanceUri']           = (string)$loadedXml->INSTANCE_URI;
+                $ozwilloConfig['instantiationSecret']   = (string)$loadedXml->INSTANTIATION_SECRET;
+                $ozwilloConfig['uri']                   = (string)$loadedXml->URI;
+                $ozwilloConfig['clientId']              = (string)$loadedXml->CLIENT_ID;
+                $ozwilloConfig['clientSecret']          = (string)$loadedXml->CLIENT_SECRET;
             }
         }
 
