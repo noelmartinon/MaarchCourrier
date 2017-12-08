7c831b5f39efe335117ff547e755d7fc908ec368
Nestor <npegane@hotmail.com>
2017-12-08 11:55:53 +0100

    FEAT #6457 watermark condition added for outgoing mail

--- apps/maarch_entreprise/indexing_searching/view_resource_controler.php
+++ apps/maarch_entreprise/indexing_searching/view_resource_controler.php
@@ -137,14 +137,20 @@ if ($s_id == '') {
                     array($s_id, $_SESSION['collection_id_choice']));
                 $res_att = $stmt->fetchObject();
                 if ($stmt->rowCount() > 0) {
+                    if($_REQUEST['watermark_outgoing']=='true'){?>
+                        <script type="text/javascript">
+                            window.location.href = '<?php
+                                echo $_SESSION['config']['businessappurl'];
+                                ?>index.php?display=true&module=attachments&page=view_attachment&res_id_master=<?php echo $s_id;?>&id=<?php echo $res_att->res_id;?>&watermark_outgoing=true'
+                        </script>
+                    <?php } else { ?>
+                        <script type="text/javascript">
+                            window.location.href = 'index.php?display=true&editingMode=true&module=attachments&page=view_attachment&res_id_master=<?php echo $s_id;?>&id=<?php echo $res_att->res_id;?>'
+                        </script>
+                    <?php 
+                        }
                     ?>
-                    <script type="text/javascript">
-                    window.location.href = '<?php
-                        echo $_SESSION['config']['businessappurl'];
-                        ?>index.php?display=true&module=attachments&page=view_attachment&res_id_master=<?php echo $s_id;?>&id=<?php echo $res_att->res_id;?>'
-                    </script>
-                    <?php
-                    exit();
+                    <?php exit();
                 }  else {
                 	$stmt = $db->query("SELECT res_id FROM res_view_attachments WHERE status <> 'DEL' and status <> 'OBS' "
                         . "and res_id_master = ? and coll_id = ? and ((attachment_type = 'converted_pdf' and (type_id = 1 or type_id = 0)) "
