67fa4cf6574e9d618f396efd89bd800e4b82ee3f
Nestor <npegane@hotmail.com>
2018-01-11 11:44:42 +0100

    FIX #6457 removed wrong id

--- apps/maarch_entreprise/indexing_searching/watermark.php
+++ apps/maarch_entreprise/indexing_searching/watermark.php
@@ -68,14 +68,14 @@ if ($watermarkTab['text'] == '') {
             $dbView = new Database();
 		    $query = " select " . $matches[1][$z] 
 		        . " as thecolumn from mlb_coll_ext where res_id = ?";
-		    $stmt = $dbView->query($query, array($_REQUEST['res_id_master']));
+		    $stmt = $dbView->query($query, array($res_id));
             $returnQuery = $stmt->fetchObject();
             $currentText = $returnQuery->thecolumn;
         } else {
 		    $dbView = new Database();
 		    $query = " select " . $matches[1][$z] 
 		        . " as thecolumn from " . $table . " where res_id = ?";
-		    $stmt = $dbView->query($query, array($res_id));
+		    $stmt = $dbView->query($query, array($s_id));
 		    $returnQuery = $stmt->fetchObject();
 		    $currentText = $returnQuery->thecolumn;
     	}
