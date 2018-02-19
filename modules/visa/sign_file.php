906d44a5ce51e47901c147d332e8c080dd5f6fd5
Alex Orluc <alex.orluc@maarch.org>
2018-02-19 11:27:25 +0100

    change font for sign + remove space beetween '/'

--- modules/visa/sign_file.php
+++ modules/visa/sign_file.php
@@ -4,11 +4,12 @@
 * See LICENCE.txt file at the root folder for more details.
 * This file is part of Maarch software.
 
+*
 * @brief   sign_file
+*
 * @author  dev <dev@maarch.org>
 * @ingroup visa
 */
-
 require_once 'core/class/class_core_tools.php';
 require_once 'core/class/class_db.php';
 require_once 'core/core_tables.php';
@@ -19,10 +20,10 @@ require_once 'core/class/class_resource.php';
 
 function writeLogIndex($EventInfo)
 {
-    $logFileOpened = fopen($_SESSION['config']['corepath'] . '/modules/visa/log/signFile_' . date('Y') . '_' . date('m'). '_' . date('d') . '.log', 'a');
-    fwrite($logFileOpened, '[' . date('d') . '/' . date('m') . '/' . date('Y')
-        . ' ' . date('H') . ':' . date('i') . ':' . date('s') . '] ' . $EventInfo
-        . "\r\n"
+    $logFileOpened = fopen($_SESSION['config']['corepath'].'/modules/visa/log/signFile_'.date('Y').'_'.date('m').'_'.date('d').'.log', 'a');
+    fwrite($logFileOpened, '['.date('d').'/'.date('m').'/'.date('Y')
+        .' '.date('H').':'.date('i').':'.date('s').'] '.$EventInfo
+        ."\r\n"
     );
     fclose($logFileOpened);
 }
@@ -43,12 +44,12 @@ if (!empty($_REQUEST['id']) && !empty($_REQUEST['collId'])) {
         $signature = $stmt->fetchObject();
         if (empty($signature)) {
             $_SESSION['error'] = _IMG_SIGN_MISSING;
-            echo "{\"status\":1, \"error\" : \"". _IMG_SIGN_MISSING ."\"}";
+            echo '{"status":1, "error" : "'._IMG_SIGN_MISSING.'"}';
             exit;
         }
 
         $docserver = \Core\Models\DocserverModel::getByTypeId(['docserver_type_id' => 'TEMPLATES', 'select' => ['path_template']]);
-        $pathToWantedSignature = $docserver[0]['path_template'] . str_replace('#', '/', $signature->signature_path) . $signature->signature_file_name;
+        $pathToWantedSignature = $docserver[0]['path_template'].str_replace('#', '/', $signature->signature_path).$signature->signature_file_name;
     } else {
         $pathToWantedSignature = $_SESSION['user']['pathToSignature'][0];
     }
@@ -58,40 +59,37 @@ if (!empty($_REQUEST['id']) && !empty($_REQUEST['collId'])) {
     if (isset($_REQUEST['isOutgoing'])) {
         if (isset($_REQUEST['isVersion'])) {
             $stmt = $db->query(
-                "select res_id_version, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from "
-                . $tableName
-                . " where attachment_type = ? and res_id_version = ?", ['outgoing_mail', $objectId]
+                'select res_id_version, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from '
+                .$tableName
+                .' where attachment_type = ? and res_id_version = ?', ['outgoing_mail', $objectId]
             );
-
         } else {
             $stmt = $db->query(
-                "select res_id, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from "
-                . $tableName
-                . " where attachment_type = ? and res_id = ?", ['outgoing_mail', $objectId]
+                'select res_id, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from '
+                .$tableName
+                .' where attachment_type = ? and res_id = ?', ['outgoing_mail', $objectId]
             );
         }
     } else {
         if (isset($_REQUEST['isVersion'])) {
             $stmt = $db->query(
-                "select res_id_version, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from "
-                . $tableName
-                . " where attachment_type NOT IN ('converted_pdf','print_folder') and res_id_version = ?", array($objectId)
+                'select res_id_version, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from '
+                .$tableName
+                ." where attachment_type NOT IN ('converted_pdf','print_folder') and res_id_version = ?", array($objectId)
             );
-
         } else {
             $stmt = $db->query(
-                "select res_id, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from "
-                . $tableName
-                . " where (attachment_type NOT IN ('converted_pdf','print_folder')) and res_id = ?", array($objectId)
+                'select res_id, format, res_id_master, title, identifier, type_id, attachment_type, dest_contact_id, dest_address_id, dest_user from '
+                .$tableName
+                ." where (attachment_type NOT IN ('converted_pdf','print_folder')) and res_id = ?", array($objectId)
             );
         }
     }
 
     if ($stmt->rowCount() < 1) {
-        echo "{\"status\":1, \"error\" : \"". _FILE . ' ' . _UNKNOWN ."\"}";
+        echo '{"status":1, "error" : "'._FILE.' '._UNKNOWN.'"}';
         exit;
         //$_SESSION['error'] = _FILE . ' ' . _UNKNOWN;
-    
     } else {
         $line = $stmt->fetchObject();
         $_SESSION['visa']['last_resId_signed']['res_id'] = $line->res_id_master;
@@ -108,58 +106,57 @@ if (!empty($_REQUEST['id']) && !empty($_REQUEST['collId'])) {
         $tmpPathToWantedSignature = $pathToWantedSignature;
         if ($core->test_service('use_date_in_signBlock', 'visa', false) == 1) {
             $infoSignFile = pathinfo($pathToWantedSignature);
-            if ($infoSignFile['extension']=='png') {
+            if ($infoSignFile['extension'] == 'png') {
                 $im2 = @imagecreatefrompng($pathToWantedSignature);
-            } else if ($infoSignFile['extension']=='jpeg' || $infoSignFile['extension']=='jpg') {
+            } elseif ($infoSignFile['extension'] == 'jpeg' || $infoSignFile['extension'] == 'jpg') {
                 $im2 = @imagecreatefromjpeg($pathToWantedSignature);
-            } else if ($infoSignFile['extension']=='gif') {
+            } elseif ($infoSignFile['extension'] == 'gif') {
                 $im2 = @imagecreatefromgif($pathToWantedSignature);
             }
-            
-            $im = imagecreatetruecolor(imagesx($im2), imagesy($im2)+30);
+
+            $im = imagecreatetruecolor(imagesx($im2), imagesy($im2) + 30);
             $white = imagecolorallocate($im, 255, 255, 255);
             $grey = imagecolorallocate($im, 128, 128, 128);
             $black = imagecolorallocate($im, 0, 0, 0);
             imagefilledrectangle($im, 0, 0, imagesx($im2), 30, $white);
 
             $stmt = $db->query(
-                "select city from entities"
-                . " where (parent_entity_id IS NULL or parent_entity_id = '') and (city IS NOT NULL or city <> '')", array()
+                'select city from entities'
+                ." where (parent_entity_id IS NULL or parent_entity_id = '') and (city IS NOT NULL or city <> '')", array()
             );
             $res = $stmt->fetchObject();
             if (!empty($res->city)) {
-                $text = $res->city.', le '.date('d / m / Y');
+                $text = $res->city.', le '.date('d/m/Y');
             } else {
-                $text = 'Le '.date('d/m/Y'); 
+                $text = 'Le '.date('d/m/Y');
             }
 
-            $font = 'modules/visa/LiberationSans-Regular.ttf';
+            $font = 'modules/visa/LibraSerifModern-Regular.otf';
             imagettftext($im, 10, 0, 10, 20, $black, $font, $text);
             imagecopy($im, $im2, 0, 30, 0, 0, imagesx($im2), imagesy($im2));
 
-            $tmpPathToWantedSignature = $_SESSION['config']['tmppath'].'tmp_file_' . $_SESSION['user']['UserId'] . '_' . rand() . '.png';
+            $tmpPathToWantedSignature = $_SESSION['config']['tmppath'].'tmp_file_'.$_SESSION['user']['UserId'].'_'.rand().'.png';
             imagepng($im, $tmpPathToWantedSignature);
         }
 
-        
         if (!file_exists($fileOnDs)) {
             echo "{\"status\":1, \"error\" : \"Fichier $fileOnDs non present\"}";
             exit;
         }
-        $cmd = "java -jar " 
-            . escapeshellarg($_SESSION['config']['corepath'] . "modules/visa/dist/SignPdf.jar") . " " 
-            . escapeshellarg($fileOnDs) . " " 
-            . escapeshellarg($tmpPathToWantedSignature) . " "
-            . escapeshellarg($_SESSION['modules_loaded']['visa']['width_blocsign']) . " " 
-            . escapeshellarg($_SESSION['modules_loaded']['visa']['height_blocsign']) . " " 
-            . escapeshellarg($_SESSION['config']['tmppath']);
+        $cmd = 'java -jar '
+            .escapeshellarg($_SESSION['config']['corepath'].'modules/visa/dist/SignPdf.jar').' '
+            .escapeshellarg($fileOnDs).' '
+            .escapeshellarg($tmpPathToWantedSignature).' '
+            .escapeshellarg($_SESSION['modules_loaded']['visa']['width_blocsign']).' '
+            .escapeshellarg($_SESSION['modules_loaded']['visa']['height_blocsign']).' '
+            .escapeshellarg($_SESSION['config']['tmppath']);
 
         //echo $cmd;
         exec($cmd);
-        
+
         $tmpFileName = pathinfo($fileOnDs, PATHINFO_BASENAME);
-        $fileExtension = "pdf";
-        
+        $fileExtension = 'pdf';
+
         include 'modules/visa/save_attach_res_from_cm.php';
         $db->query('UPDATE listinstance set signatory = TRUE WHERE listinstance_id = (SELECT listinstance_id FROM listinstance WHERE res_id = ? AND item_id = ? AND difflist_type = ? AND process_date is null ORDER BY listinstance_id LIMIT 1)', [$objectResIdMaster, $_SESSION['user']['UserId'], 'VISA_CIRCUIT']);
 
