a3aa9f8ec396b0461c73cffcbdd1746654829bce
florian.azizian <florian.azizian@maarch.org>
2018-09-07 11:15:47 +0100

    FIX #8237 can not create moral contact

--- src/app/contact/models/ContactModelAbstract.php
+++ src/app/contact/models/ContactModelAbstract.php
@@ -41,7 +41,7 @@ class ContactModelAbstract
 
     public static function create(array $aArgs)
     {
-        ValidatorModel::notEmpty($aArgs, ['firstname', 'lastname', 'contactType', 'isCorporatePerson', 'userId', 'entityId']);
+        ValidatorModel::notEmpty($aArgs, ['contactType', 'isCorporatePerson', 'userId', 'entityId']);
         ValidatorModel::intVal($aArgs, ['contactType']);
         ValidatorModel::stringType($aArgs, [
             'firstname', 'lastname', 'isCorporatePerson', 'society',
