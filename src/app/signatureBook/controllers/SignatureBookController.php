b17d69d980d13f1a573425a5d7d2186b297e06c6
florian.azizian <florian.azizian@maarch.org>
2019-04-02 10:16:05 +0100

    FIX  converted pdf in signatory book

--- src/app/signatureBook/controllers/SignatureBookController.php
+++ src/app/signatureBook/controllers/SignatureBookController.php
@@ -192,7 +192,7 @@ class SignatureBookController
                 $isVersion = false;
             }
 
-            $convertedAttachment = ConvertPdfController::getConvertedPdfById(['select' => ['docserver_id', 'path', 'filename'], 'resId' => $value['res_id'], 'collId' => 'attachments_coll', 'isVersion' => $isVersion]);
+            $convertedAttachment = ConvertPdfController::getConvertedPdfById(['select' => ['docserver_id', 'path', 'filename'], 'resId' => $realId, 'collId' => 'attachments_coll', 'isVersion' => $isVersion]);
 
             if (empty($convertedAttachment['errors'])) {
                 $isConverted = true;
@@ -201,12 +201,12 @@ class SignatureBookController
             }
 
             $documents[] = [
-                'res_id'        => $value['res_id'],
+                'res_id'        => $realId,
                 'title'         => $value['title'],
                 'format'        => $value['format'],
                 'isConverted'   => $isConverted,
-                'viewerLink'    => "../../rest/res/{$resId}/attachments/{$value['res_id']}/content",
-                'thumbnailLink' => "rest/res/{$resId}/attachments/{$value['res_id']}/thumbnail"
+                'viewerLink'    => "../../rest/res/{$resId}/attachments/{$realId}/content",
+                'thumbnailLink' => "rest/res/{$resId}/attachments/{$realId}/thumbnail"
             ];
         }
 
@@ -250,23 +250,26 @@ class SignatureBookController
                 continue;
             }
 
-            $collId = '';
             $realId = 0;
-            $isVersion = 'false';
             if ($value['res_id'] == 0) {
-                $collId = 'version_attachments_coll';
                 $realId = $value['res_id_version'];
-                $isVersion = 'true';
+                $isVersion = true;
             } elseif ($value['res_id_version'] == 0) {
-                $collId = 'attachments_coll';
                 $realId = $value['res_id'];
-                $isVersion = 'false';
+                $isVersion = false;
             }
 
-            $viewerId = $realId;
+            $viewerId       = $realId;
             $viewerNoSignId = $realId;
-            $pathToFind = $value['path'] . str_replace(strrchr($value['filename'], '.'), '.pdf', $value['filename']);
-            $isConverted = false;
+            $pathToFind     = $value['path'] . str_replace(strrchr($value['filename'], '.'), '.pdf', $value['filename']);
+            $isConverted    = false;
+
+            $convertedAttachment = ConvertPdfController::getConvertedPdfById(['select' => [1], 'resId' => $realId, 'collId' => 'attachments_coll', 'isVersion' => $isVersion]);
+
+            if (empty($convertedAttachment['errors'])) {
+                $isConverted = true;
+            }
+
             foreach ($attachments as $tmpKey => $tmpValue) {
                 if (strpos($value['format'], 'xl') !== 0 && $value['format'] != 'pptx' && $tmpValue['attachment_type'] == 'converted_pdf' && ($tmpValue['path'] . $tmpValue['filename'] == $pathToFind)) {
                     if ($value['status'] != 'SIGN') {
