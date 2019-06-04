773c809fe2bef8c2b2275e875f16f58d9289d640
florian.azizian <florian.azizian@maarch.org>
2019-06-04 16:20:59 +0100

    FIX #10764 TIME 0:30 migration added_by_entity with deleted entity

--- sql/1904.sql
+++ sql/1904.sql
@@ -258,6 +258,8 @@ END$$;
 ALTER TABLE res_mark_as_read DROP COLUMN IF EXISTS coll_id;
 
 UPDATE listinstance SET added_by_entity = 'superadmin' WHERE added_by_user = 'superadmin';
+UPDATE listinstance SET added_by_entity = 'superadmin' WHERE listinstance_id IN 
+	(SELECT listinstance_id FROM listinstance LEFT JOIN entities ON listinstance.added_by_entity = entities.entity_id WHERE entities.entity_id IS null);
 UPDATE listinstance SET added_by_entity =
     (SELECT entity_id FROM users_entities WHERE users_entities.user_id = listinstance.added_by_user AND primary_entity = 'Y')
 WHERE added_by_entity IS NULL OR added_by_entity = '';
