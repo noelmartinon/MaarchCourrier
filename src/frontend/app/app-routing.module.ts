d5113fcd1632f838e420ba418345d95f24b93757
Alex ORLUC <alex.orluc@maarch.org>
2019-07-04 13:25:37 +0200

    FEAT #10938 TIME 0:30 save v2 route if v1 location

--- src/frontend/app/app-routing.module.ts
+++ src/frontend/app/app-routing.module.ts
@@ -9,19 +9,20 @@ import { HomeComponent }                    from './home/home.component';
 import { BasketListComponent }              from './list/basket-list.component';
 import { SignatureBookComponent }           from './signature-book.component';
 import { SaveNumericPackageComponent }      from './save-numeric-package.component';
+import { AppGuard }                         from '../service/app.guard';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             { path: 'activate-user', component: ActivateUserComponent},
             { path: 'password-modification', component: PasswordModificationComponent },
-            { path: 'profile', component: ProfileComponent },
-            { path: 'about-us', component: AboutUsComponent },
-            { path: 'home', component: HomeComponent },
-            { path: 'basketList/users/:userSerialId/groups/:groupSerialId/baskets/:basketId', component: BasketListComponent },
-            { path: 'saveNumericPackage', component: SaveNumericPackageComponent },
-            { path: 'signatureBook/users/:userId/groups/:groupId/baskets/:basketId/resources/:resId', component: SignatureBookComponent },
-            { path: '**',   redirectTo: 'home', pathMatch: 'full' },
+            { path: 'profile', canActivate: [AppGuard], component: ProfileComponent },
+            { path: 'about-us', canActivate: [AppGuard], component: AboutUsComponent },
+            { path: 'home', canActivate: [AppGuard],  component: HomeComponent },
+            { path: 'basketList/users/:userSerialId/groups/:groupSerialId/baskets/:basketId', canActivate: [AppGuard], component: BasketListComponent },
+            { path: 'saveNumericPackage', canActivate: [AppGuard], component: SaveNumericPackageComponent },
+            { path: 'signatureBook/users/:userId/groups/:groupId/baskets/:basketId/resources/:resId', canActivate: [AppGuard],component: SignatureBookComponent },
+            { path: '**',  redirectTo: 'home', pathMatch: 'full' },
         ], { useHash: true }),
     ],
     exports: [
