5dc364925ba71f2a3be28f3a50b590b99fd3204c
Damien <damien.burel@maarch.org>
2020-03-26 11:05:49 +0100

    FEAT #10261 TIME 0:05 Basic auth with suspend users can not log in

--- src/core/models/AuthenticationModel.php
+++ src/core/models/AuthenticationModel.php
@@ -29,8 +29,8 @@ class AuthenticationModel
         $aReturn = DatabaseModel::select([
             'select'    => ['password'],
             'table'     => ['users'],
-            'where'     => ['lower(user_id) = lower(?)', 'status != ?', '(locked_until is null OR locked_until < CURRENT_TIMESTAMP)'],
-            'data'      => [$args['userId'], 'DEL']
+            'where'     => ['lower(user_id) = lower(?)', 'status = ?', 'enabled = ?', '(locked_until is null OR locked_until < CURRENT_TIMESTAMP)'],
+            'data'      => [$args['userId'], 'OK', 'Y']
         ]);
 
         if (empty($aReturn[0])) {
