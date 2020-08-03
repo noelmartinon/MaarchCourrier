16aea10c85d1c67748811e82eaf1f8f39a1e6e67
florian.azizian <florian.azizian@maarch.org>
2020-08-03 19:08:33 +0200

    FIX #14554 TIME 3:45 fix change order of contact custom fields + fix RAZ all custom fields for contact and resources + check jsonb_set set to null

--- src/app/entity/controllers/EntityController.php
+++ src/app/entity/controllers/EntityController.php
@@ -357,7 +357,8 @@ class EntityController
 
         GroupModel::update([
             'postSet'   => ['indexation_parameters' => "jsonb_set(indexation_parameters, '{entities}', (indexation_parameters->'entities') - '{$entity['id']}')"],
-            'where'     => ['1=1']
+            'where'     => ["indexation_parameters->'entities' @> ?"],
+            'data'      => ['"'.$entity['id'].'"']
         ]);
 
         EntityModel::delete(['where' => ['entity_id = ?'], 'data' => [$aArgs['id']]]);
@@ -456,7 +457,8 @@ class EntityController
         //GroupIndexing
         GroupModel::update([
             'postSet'   => ['indexation_parameters' => "jsonb_set(indexation_parameters, '{entities}', (indexation_parameters->'entities') - '{$dyingEntity['id']}')"],
-            'where'     => ['1=1']
+            'where'     => ["indexation_parameters->'entities' @> ?"],
+            'data'      => ['"'.$dyingEntity['id'].'"']
         ]);
 
 
