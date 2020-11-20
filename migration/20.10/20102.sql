f2233dc58ed6b515f1cb975bc0ffce3ee9fec397
Guillaume Heurtier <guillaume.heurtier@maarch.org>
2020-11-20 16:16:41 +0100

    FEAT #14534 TIME 0:25 improved error messages in visa circuit

--- migration/20.10/20102.sql
+++ migration/20.10/20102.sql
@@ -8,10 +8,9 @@
 --DATABASE_BACKUP|parameters
 
 DELETE FROM parameters WHERE id = 'minimumVisaRole';
-INSERT INTO parameters (id, param_value_int) VALUES ('minimumVisaRole', 0);
+INSERT INTO parameters (id, description, param_value_int) VALUES ('minimumVisaRole', 'Nombre minimum de viseur dans les circuits de visa (0 pour désactiver)', 0);
 
 DELETE FROM parameters WHERE id = 'maximumSignRole';
-INSERT INTO parameters (id, param_value_int) VALUES ('maximumSignRole', 0);
-
+INSERT INTO parameters (id, description, param_value_int) VALUES ('maximumSignRole', 'Nombre maximum de signataires dans les circuits de visa (0 pour désactiver)', 0);
 
 UPDATE parameters SET param_value_string = '20.10.2' WHERE id = 'database_version';
