e02258c5abb986247a8d78b3b8c96ccd09db32ca
Guillaume Heurtier <guillaume.heurtier@maarch.org>
2021-01-07 12:09:48 +0100

    FIX #15016 TIME 0:40 fix signatory not kept when updating circuit + fix signatory not in list history

--- migration/20.10/20104.sql
+++ migration/20.10/20104.sql
@@ -8,5 +8,7 @@
 
 ALTER TABLE listinstance_history_details DROP COLUMN IF EXISTS requested_signature;
 ALTER TABLE listinstance_history_details ADD COLUMN requested_signature boolean default false;
+ALTER TABLE listinstance_history_details DROP COLUMN IF EXISTS signatory;
+ALTER TABLE listinstance_history_details ADD COLUMN signatory BOOLEAN DEFAULT FALSE;
 
 UPDATE parameters SET param_value_string = '20.10.4' WHERE id = 'database_version';
