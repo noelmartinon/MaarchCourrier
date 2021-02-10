443a0dafd452354f873ca68ab34c6539cdf2aaad
Alex ORLUC <alex.orluc@maarch.org>
2021-02-10 09:38:38 +0100

    FIX #15155 TIME 0:10 action send to record management refactoring

--- src/frontend/app/actions/send-to-record-management-action/send-to-record-management.component.ts
+++ src/frontend/app/actions/send-to-record-management-action/send-to-record-management.component.ts
@@ -1,148 +1,109 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { TranslateService } from '@ngx-translate/core';
 import { FunctionsService } from '@service/functions.service';
 import { NotificationService } from '@service/notification/notification.service';
-import { error } from 'jquery';
 import { of } from 'rxjs';
 import { catchError, finalize, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-send-to-record-management',
     templateUrl: 'send-to-record-management.component.html',
-    styleUrls: ['send-to-record-management.component.scss']
+    styleUrls: ['send-to-record-management.component.scss'],
+    animations: [
+        trigger('detailExpand', [
+            state('collapsed', style({ height: '0px', minHeight: '0' })),
+            state('expanded', style({ height: '*' })),
+            transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+        ]),
+    ],
 })
 export class SendToRecordManagementComponent implements OnInit {
 
     loading: boolean = false;
-    checking: boolean = false;
+    checking: boolean = true;
+
+    resources: any[] = [];
     resourcesErrors: any[] = [];
+    criticalError: any = null;
+
+    dataSource = null;
+
+    senderArchiveEntity: string = '';
+
     recipientArchiveEntities = [];
+    entityArchiveRecipient: string = null;
+
     archivalAgreements = [];
+    archivalAgreement: string = null;
 
-    descriptionLevels = [
-        {
-            id: 'Class',
-            label: 'Classe'
-        },
-        {
-            id: 'Collection',
-            label: 'Collection'
-        },
-        {
-            id: 'File',
-            label: 'Dossier'
-        },
-        {
-            id: 'Fonds',
-            label: 'Fonds'
-        },
-        {
-            id: 'Item',
-            label: 'Pièce'
-        },
-        {
-            id: 'RecordGrp',
-            label: 'Groupe de documents'
-        },
-        {
-            id: 'Series',
-            label: 'Série organique'
-        },
-        {
-            id: 'Subfonds',
-            label: 'Sous-fonds'
-        },
-        {
-            id: 'SubGrp',
-            label: 'Sous-groupe de documents'
-        },
-        {
-            id: 'Subseries',
-            label: 'Sous-série organique'
-        }
-    ];
+    columnsToDisplay = ['chrono', 'subject', 'slipId', 'producerService', 'retentionRule', 'retentionFinalDisposition', 'countArchives'];
 
-    actionFormGroup: FormGroup;
     archives: any[] = [];
     folders: any = [];
     linkedResources: any = [];
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
 
     constructor(
         public translate: TranslateService,
         public http: HttpClient,
         private notify: NotificationService,
         public dialogRef: MatDialogRef<SendToRecordManagementComponent>,
-        private _formBuilder: FormBuilder,
         @Inject(MAT_DIALOG_DATA) public data: any,
         public functions: FunctionsService
-    ) {
-        this.actionFormGroup = this._formBuilder.group({
-            folder :  [''],
-            packageName: ['', Validators.required],
-            slipId: [{value: '', disabled: true}, Validators.required],
-            slipDate: [new Date(), Validators.required],
-            archivalAgreement: [{value: '', disabled: false}, Validators.required],
-            entityArchiveRecipient: [{value: '', disabled: false}, Validators.required],
-            entityLabelTransferEntity: [{value: '', disabled: true}, Validators.required],
-            producerTransferEntity: [{value: '', disabled: true}, Validators.required],
-            senderArchiveEntity: [{value: '', disabled: true}, Validators.required],
-            archiveId: [{value: '', disabled: true}, Validators.required],
-            archiveDescriptionLevel: [{value: 'File', disabled: false}, Validators.required],
-            doctype: [{value: '', disabled: true}, Validators.required],
-            entityRetentionRule: [{value: '', disabled: true}, Validators.required],
-            doctypeRetentionFinalDisposition: [{value: '', disabled: true}, Validators.required]
-        });
-    }
+    ) { }
 
     ngOnInit(): void {
-        if (this.data.resIds.length === 1) {
-            this.getData();
-        } else {
-            this.resourcesErrors.push({lang : 'onlySingleResourceAllowed'});
-        }
+        this.getData();
     }
 
     getData() {
-        this.checking = true;
         this.http.post(`../rest/resourcesList/users/${this.data.userId}/groups/${this.data.groupId}/baskets/${this.data.basketId}/actions/${this.data.action.id}/checkSendToRecordManagement`, { resources: this.data.resIds }).pipe(
             tap((data: any) => {
-                this.archives = data.archiveUnits;
-                this.archives.forEach((element: any) => {
-                    element.type = this.translate.instant('lang.' + element.type);
+                this.resourcesErrors = data.errors;
+
+                Object.keys(data.success).forEach((resId: any, index: number) => {
+                    if (Object.keys(data.success).length === 1) {
+                        this.linkedResources = data.success[resId].additionalData.linkedResources;
+                        this.folders = data.success[resId].additionalData.folders;
+                    }
+                    this.resources.push({
+                        resId: resId,
+                        chrono: data.success[resId].data.metadata.alt_identifier,
+                        subject: data.success[resId].data.metadata.subject,
+                        slipId: data.success[resId].data.slipInfo.slipId,
+                        archiveId: data.success[resId].data.slipInfo.archiveId,
+                        retentionFinalDisposition: data.success[resId].data.doctype.retentionFinalDisposition,
+                        archives: data.success[resId].archiveUnits,
+                        doctype: data.success[resId].data.doctype.label,
+                        retentionRule: data.success[resId].data.doctype.retentionRule,
+                        producerService: data.success[resId].data.entity.producerService,
+                        entity: data.success[resId].data.entity.label,
+                        folder: data.success[resId].additionalData.folders.length > 0 ? data.success[resId].additionalData.folders[0] : null,
+                        countArchives: data.success[resId].archiveUnits.length
+                    });
                 });
-
-                this.linkedResources = data.additionalData.linkedResources;
-                this.folders = data.additionalData.folders;
-
-                this.recipientArchiveEntities = data.recipientArchiveEntities;
                 this.archivalAgreements = data.archivalAgreements;
-                this.actionFormGroup = this._formBuilder.group({
-                    folder : [this.folders.length > 0 ? this.folders[0].id : null],
-                    packageName: ['', Validators.required],
-                    slipId: [{value: data.data.slipInfo.slipId, disabled: true}, Validators.required],
-                    slipDate: [new Date(), Validators.required],
-                    archivalAgreement: [{value: '', disabled: false}, Validators.required],
-                    entityArchiveRecipient: [{value: '', disabled: false}, Validators.required],
-                    entityLabelTransferEntity: [{value: data.data.entity.label, disabled: true}, Validators.required],
-                    producerTransferEntity: [{value: data.data.entity.producerService, disabled: true}, Validators.required],
-                    senderArchiveEntity: [{value: data.data.entity.senderArchiveEntity, disabled: true}, Validators.required],
-                    archiveId: [{value: data.data.slipInfo.archiveId, disabled: true}, Validators.required],
-                    archiveDescriptionLevel: [{value: 'File', disabled: false}, Validators.required],
-                    doctype: [{value: data.data.doctype.label, disabled: true}, Validators.required],
-                    entityRetentionRule: [{value: data.data.doctype.retentionRule, disabled: true}, Validators.required],
-                    doctypeRetentionFinalDisposition: [{value: data.data.doctype.retentionFinalDisposition, disabled: true}, Validators.required],
-                });
+                this.recipientArchiveEntities = data.recipientArchiveEntities;
+                this.senderArchiveEntity = data.senderArchiveEntity;
+                this.checking = false;
+                setTimeout(() => {
+                    this.dataSource = new MatTableDataSource(this.resources);
+                    this.dataSource.paginator = this.paginator;
+                }, 0);
             }),
-            finalize(() => this.checking = false),
             catchError((err: any) => {
                 if (!this.functions.empty(err.error.lang)) {
-                    this.resourcesErrors.push(this.translate.instant('lang.' + err.error.lang));
+                    this.criticalError = this.translate.instant('lang.' + err.error.lang)
                 } else {
-                    this.resourcesErrors.push(err.error.errors);
+                    this.criticalError = err.error.errors;
                 }
+                this.checking = false;
                 return of(false);
             })
         ).subscribe();
@@ -156,33 +117,40 @@ export class SendToRecordManagementComponent implements OnInit {
     }
 
     executeAction(mode: string) {
-        const realResSelected: number[] = this.data.resIds;
+        const realResSelected: number[] = this.resources.map((item: any) => item.resId);
 
         this.http.put(this.data.processActionRoute, { resources: realResSelected, data: this.formatData(mode) }).pipe(
             tap((data: any) => {
                 if (mode === 'download' && !this.functions.empty(data.data.encodedFile)) {
                     const downloadLink = document.createElement('a');
+                    let filenameDetail: string;
                     downloadLink.href = `data:application/zip;base64,${data.data.encodedFile}`;
-                    let today: any;
-                    let dd: any;
-                    let mm: any;
-                    let yyyy: any;
+                    if (this.data.resIds.length === 1) {
+                        filenameDetail = this.data.resource.chrono.split(' ').join('_');
+                    } else {
+                        let today: any;
+                        let dd: any;
+                        let mm: any;
+                        let yyyy: any;
 
-                    today = new Date();
-                    dd = today.getDate();
-                    mm = today.getMonth() + 1;
-                    yyyy = today.getFullYear();
+                        today = new Date();
+                        dd = today.getDate();
+                        mm = today.getMonth() + 1;
+                        yyyy = today.getFullYear();
 
-                    if (dd < 10) {
-                        dd = '0' + dd;
+                        if (dd < 10) {
+                            dd = '0' + dd;
+                        }
+                        if (mm < 10) {
+                            mm = '0' + mm;
+                        }
+                        filenameDetail = dd + '-' + mm + '-' + yyyy;
                     }
-                    if (mm < 10) {
-                        mm = '0' + mm;
-                    }
-                    today = dd + '-' + mm + '-' + yyyy;
-                    downloadLink.setAttribute('download', 'seda_package_' + today + '.zip');
+
+                    downloadLink.setAttribute('download', 'seda_package_' + filenameDetail + '.zip');
                     document.body.appendChild(downloadLink);
                     downloadLink.click();
+
                     this.dialogRef.close('success');
                 } else if (!data) {
                     this.dialogRef.close('success');
@@ -200,31 +168,28 @@ export class SendToRecordManagementComponent implements OnInit {
     }
 
     formatData(mode: string) {
-        const dataToSend = {};
-        Object.keys(this.actionFormGroup.controls).forEach(element => {
-            dataToSend[element] = this.actionFormGroup.controls[element].value;
-        });
-        dataToSend['archives'] = this.archives.map((archive: any) => {
-            return {
-                id: archive.id,
-                descriptionLevel : archive.descriptionLevel
-            };
-        });
-        dataToSend['actionMode'] = mode;
+        const dataToSend = {
+            'archivalAgreement': this.archivalAgreement,
+            'entityArchiveRecipient': this.entityArchiveRecipient,
+            'folder': this.resources.length === 1 && this.folders.length > 0 ? this.resources[0].folder.id : null,
+            'actionMode': mode
+        };
         return dataToSend;
     }
 
-    archivalAgreementSelected(ev: any) {
-        const archivalAgreement = this.archivalAgreements.filter((element: any) => element.id === ev.value);
-        this.actionFormGroup.patchValue({entityArchiveRecipient: archivalAgreement[0].archiveEntityRegNumber});
+    getFolderLabel(folder: any) {
+        if (this.resources.length === 1) {
+            return this.folders.find((item: any) => item.id === folder);
+        } else {
+            return folder;
+        }
     }
 
-    entityArchiveRecipientSelected(ev: any) {
-        if (!this.functions.empty(this.actionFormGroup.get('archivalAgreement').value) && !this.functions.empty(ev.value)) {
-            const archivalAgreement = this.archivalAgreements.filter((element: any) => element.id === this.actionFormGroup.get('archivalAgreement').value && element.archiveEntityRegNumber === ev.value);
-            if (archivalAgreement.length === 0) {
-                this.actionFormGroup.patchValue({archivalAgreement: null});
-            }
+    isValid() {
+        if (this.resources.length === 1 && this.folders.length > 0) {
+            return !this.functions.empty(this.archivalAgreement) && !this.functions.empty(this.entityArchiveRecipient) && !this.functions.empty(this.resources[0].folder);
+        } else {
+            return !this.functions.empty(this.archivalAgreement) && !this.functions.empty(this.entityArchiveRecipient);
         }
     }
 }
