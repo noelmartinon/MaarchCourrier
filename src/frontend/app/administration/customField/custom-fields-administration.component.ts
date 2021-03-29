491cbc375e7aad09fb266b1cae7ca0ccd4f2bdcf
Guillaume Heurtier <guillaume.heurtier@maarch.org>
2021-03-29 14:52:13 +0200

    FIX #16699 TIME 0:05 fix switch sql mode in custom fields admin

--- src/frontend/app/administration/customField/custom-fields-administration.component.ts
+++ src/frontend/app/administration/customField/custom-fields-administration.component.ts
@@ -241,6 +241,7 @@ export class CustomFieldsAdministrationComponent implements OnInit {
 
     switchSQLMode(custom: any) {
         custom.SQLMode = !custom.SQLMode;
+        const oldValues = custom.values;
         if (custom.SQLMode) {
             custom.values = {
                 key: 'id',
@@ -255,6 +256,10 @@ export class CustomFieldsAdministrationComponent implements OnInit {
         } else {
             custom.values = [];
         }
+        if (!this.functionsService.empty(custom.oldValues)) {
+            custom.values = custom.oldValues;
+        }
+        custom.oldValues = oldValues;
     }
 
     getTables() {
