944911cae4eed8abfb88bbe53f98447e81d1acf0
Damien <damien.burel@maarch.org>
2021-03-30 17:48:22 +0200

    FEAT #16699 TIME 4:20 Improve get correspondents when mailing

--- src/frontend/app/attachments/attachment-create/attachment-create.component.ts
+++ src/frontend/app/attachments/attachment-create/attachment-create.component.ts
@@ -126,15 +126,13 @@ export class AttachmentCreateComponent implements OnInit {
                     let contact: any = '';
                     if (data.categoryId === 'outgoing') {
                         if (!this.functions.empty(data.recipients) && data.recipients.length > 0) {
-                            data.recipients = data.recipients.filter((contact: any) => contact.type !== 'entity');
-                            await this.getContacts(data.recipients);
-                            contact = !this.functions.empty(data.recipients) ? [{ id: this.resourceContacts[0].id, type: this.resourceContacts[0].type }] : '';
+                            await this.getCorrespondents('recipients');
+                            contact = !this.functions.empty(this.resourceContacts) ? [{ id: this.resourceContacts[0].id, type: this.resourceContacts[0].type }] : '';
                         }
                     } else {
                         if (!this.functions.empty(data.senders) && data.senders.length > 0) {
-                            data.senders = data.senders.filter((contact: any) => contact.type !== 'entity');
-                            await this.getContacts(data.senders);
-                            contact = !this.functions.empty(data.senders) ? [{ id: this.resourceContacts[0].id, type: this.resourceContacts[0].type }] : '';
+                            await this.getCorrespondents('senders');
+                            contact = !this.functions.empty(this.resourceContacts) ? [{ id: this.resourceContacts[0].id, type: this.resourceContacts[0].type }] : '';
                         }
                     }
 
@@ -156,12 +154,46 @@ export class AttachmentCreateComponent implements OnInit {
                     if (this.canSendMass()) {
                         this.toggleSendMass();
                     }
-
                     resolve(true);
                 }),
                 catchError((err: any) => {
                     this.notify.handleSoftErrors(err);
                     this.dialogRef.close('');
+                    return of(false);
+                })
+            ).subscribe();
+        });
+    }
+
+    async getCorrespondents(type) {
+        this.resourceContacts = [];
+        return new Promise((resolve, reject) => {
+            this.http.get(`../rest/resources/${this.data.resIdMaster}/contacts?type=${type}`).pipe(
+                tap((correspondents: any) => {
+                    var correspondents = correspondents.contacts.filter((contact: any) => contact.type !== 'entity');
+
+                    correspondents.forEach((elem) => {
+                        if (elem.type === 'contact') {
+                            this.resourceContacts.push({
+                                id: elem.id,
+                                type: 'contact',
+                                label: this.contactService.formatContact(elem)
+                            });
+                        } else if (elem.type === 'user') {
+                            this.resourceContacts.push({
+                                id: elem.id,
+                                type: 'user',
+                                label: `${elem.firstname} ${elem.lastname}`
+                            });
+                        }
+                    });
+
+                    this.resourceContacts = this.sortPipe.transform(this.resourceContacts, 'label');
+                    resolve(true);
+                }),
+                catchError((err: any) => {
+                    this.notify.handleSoftErrors(err);
+                    resolve(false);
                     return of(false);
                 })
             ).subscribe();
