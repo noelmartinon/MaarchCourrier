c602b625f619775b95197057aea6c7f330e83afb
Alex ORLUC <alex.orluc@maarch.org>
2021-04-21 16:42:30 +0200

    FEAT #16847 TIME 1:50 add select page

---
+++ src/frontend/plugins/list/select-page/select-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+    selector: 'app-select-page',
+    templateUrl: 'select-page.component.html',
+    styleUrls: ['select-page.component.scss'],
+})
+export class SelectPageComponent implements OnInit {
+
+    @Input() paginator: MatPaginator;
+
+    constructor() { }
+
+    ngOnInit() {
+        console.log(this.paginator);
+    }
+
+    counter(i: number) {
+        return new Array(i);
+    }
+
+    goToPage(page: number) {
+        this.paginator.pageIndex = page;
+        this.paginator.page.next({
+            pageIndex: page,
+            pageSize: this.paginator.pageSize,
+            length: this.paginator.length
+        });
+    }
+
+}
