a7dba92cb5b6656e96b783a1bceff3e390cb6440
hamza.hramchi <hamza.hramchi@xelians.fr>
2021-04-23 19:18:38 +0200

    FEAT #00512 TIME 1:15 improve autocompletion display + toggle selection + change date format

--- src/frontend/app/profile/absModal/abs-modal.component.ts
+++ src/frontend/app/profile/absModal/abs-modal.component.ts
@@ -269,11 +269,18 @@ export class AbsModalComponent implements OnInit {
     }
 
     allSelected() {
-        return this.baskets.filter((item: any) => item.selected).length === this.baskets.filter((item: any) => !this.isRedirectedBasket(item)).length;
+        const basketsNotSelected: number = this.baskets.filter((basket: any) => !this.isRedirectedBasket(basket)).length;
+        const hasRedirectedBasket: boolean = this.baskets.filter((basket: any) => this.isRedirectedBasket(basket)).length >= 1;
+        if (hasRedirectedBasket) {
+            return hasRedirectedBasket && basketsNotSelected === this.baskets.filter((item: any) => item.selected).length;
+        } else {
+            return this.baskets.filter((item: any) => item.selected).length === this.baskets.filter((item: any) => !this.isRedirectedBasket(item)).length;
+        }
     }
 
     oneOrMoreSelected() {
-        return this.baskets.filter((item: any) => item.selected).length > 0 && !this.allSelected();
+        const basketRedirected: number = this.baskets.filter((basket: any) => this.isRedirectedBasket(basket)).length;
+        return (this.baskets.filter((item: any) => item.selected).length > 0 || basketRedirected >= 1) && !this.allSelected();
     }
 
     getAbsenceInfo() {
