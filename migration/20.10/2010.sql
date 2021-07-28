d78449b8f1322480914d7b334cd68f8721577747
Lionel Marie-Louise <lionel.marie-louise@maarch.org>
2021-07-28 18:21:54 +0200

    FEAT #15527 - Record templates: associate one or more entries of a 'Select' type field

--- migration/20.10/2010.sql
+++ migration/20.10/2010.sql
@@ -49,6 +49,9 @@ END$$;
 /*INDEXING_MODELS_FIELDS*/
 ALTER TABLE indexing_models_fields DROP COLUMN IF EXISTS enabled;
 ALTER TABLE indexing_models_fields ADD COLUMN enabled BOOLEAN NOT NULL DEFAULT TRUE;
+-- SGAMI-SO DEBUT
+ALTER TABLE indexing_models_fields ADD COLUMN allowed_values jsonb;
+-- SGAMI-SO FIN
 ALTER TABLE indexing_models DROP COLUMN IF EXISTS mandatory_file;
 ALTER TABLE indexing_models ADD COLUMN mandatory_file BOOLEAN NOT NULL DEFAULT FALSE;
 
