5e58d4120812d2485e1886d520d48c55aed6ea2a
Guillaume Heurtier <guillaume.heurtier@maarch.org>
2021-10-18 10:11:52 +0200

    FIX #16305 TIME 0:35 get duplicate contacts on firstname/lastname

--- src/core/controllers/AutoCompleteController.php
+++ src/core/controllers/AutoCompleteController.php
@@ -616,6 +616,30 @@ class AutoCompleteController
         return $response->withJson($contacts);
     }
 
+    public static function getContactsByName(Request $request, Response $response)
+    {
+        $queryParams = $request->getQueryParams();
+
+        if (!Validator::stringType()->notEmpty()->validate($queryParams['search'])) {
+            return $response->withStatus(400)->withJson(['errors' => 'Query params search is empty']);
+        }
+
+        $fields = AutoCompleteController::getUnsensitiveFieldsForRequest(['fields' => ['firstname', 'lastname']]);
+        $contacts = ContactModel::get([
+            'select'    => [
+                'id', 'company', 'firstname', 'lastname', 'address_number as "addressNumber"', 'address_street as "addressStreet"',
+                'address_additional1 as "addressAdditional1"', 'address_additional2 as "addressAdditional2"', 'address_postcode as "addressPostcode"',
+                'address_town as "addressTown"', 'address_country as "addressCountry"'
+            ],
+            'where'     => ['enabled = ?', $fields],
+            'data'      => [true, $queryParams['search'] . '%', $queryParams['search'] . '%'],
+            'orderBy'   => ['company', 'lastname'],
+            'limit'     => AutoCompleteController::TINY_LIMIT
+        ]);
+
+        return $response->withJson($contacts);
+    }
+
     public static function getBanAddresses(Request $request, Response $response)
     {
         $data = $request->getQueryParams();
