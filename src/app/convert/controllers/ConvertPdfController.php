dd0da137e77e3b36ba770a88a57a88022d4f5321
Quentin RIBAC <quentin.ribac@xelians.fr>
2021-12-01 16:48:25 +0100

    FIX #18831 TIME 0 revert commit on wrong branch

--- src/app/convert/controllers/ConvertPdfController.php
+++ src/app/convert/controllers/ConvertPdfController.php
@@ -48,7 +48,7 @@ class ConvertPdfController
             $xvfb = !empty($outputWhereIs[1]);
 
             if ($xvfb) {
-                $command = 'xvfb-run -a -e /dev/stderr ' . $command;
+                $command = 'xvfb-run ' . $command;
             } else {
                 $command = 'export DISPLAY=:0 && ' . $command;
             }
