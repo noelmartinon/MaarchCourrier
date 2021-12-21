f79465b6fa61183fdde03885a484a54563e35d27
Quentin RIBAC <quentin.ribac@xelians.fr>
2021-12-21 15:01:15 +0100

    FEAT #16988 TIME 0:10 using resId if subject and chrono are empty

--- src/app/resource/controllers/FolderPrintController.php
+++ src/app/resource/controllers/FolderPrintController.php
@@ -567,6 +567,10 @@ class FolderPrintController
                     $resource['altIdentifier'] = $document['alt_identifier'];
                     $resource['subject']       = $document['subject'];
                 }
+                if (empty($resource['altIdentifier'] . $resource['subject'])) {
+                    $resource['altIdentifier'] = 'MAARCH';
+                    $resource['subject']       = $resource['resId'];
+                }
                 $filePathOnTmp = trim($tmpDir . TextFormatModel::formatFilename([
                     'filename'  => $resource['altIdentifier'] . '_' . $resource['subject'],
                     'maxLength' => 100
@@ -582,7 +586,7 @@ class FolderPrintController
                 if (!file_exists($filePathOnTmp)) {
                     return $response->withStatus(500)->withJson(['errors' => 'Merged PDF file not created']);
                 }
-                $folderPrintPaths[] = str_replace('//', '/', $filePathOnTmp);
+                $folderPrintPaths[] = $filePathOnTmp;
             }
         }
 
