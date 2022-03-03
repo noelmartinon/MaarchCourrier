8c8c555ae10d39ca6b2ae6498ad159af6266c075
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2022-03-03 17:23:46 +0100

    FIX #20008 TIME 0:12 display parent entity in case of uniqueness

--- src/frontend/app/diffusions/diffusions-list.component.ts
+++ src/frontend/app/diffusions/diffusions-list.component.ts
@@ -535,7 +535,7 @@ export class DiffusionsListComponent implements OnInit {
     }
 
     hasEmptyDest() {
-        return this.diffList["dest"].items.length === 0;
+        return !this.functions.empty(this.diffList) && this.diffList['dest'].items.length === 0;
     }
 
     isEmptyList() {
