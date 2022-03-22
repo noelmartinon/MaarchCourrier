9c211794f4fdc4e8a11974b1c8dad0375d1a8864
Jean-Laurent <jean-laurent.duzant@xelians.fr>
2022-03-22 15:38:24 +0100

    FIX #19971 TIME 0:05 use SORT_REGULAR : Compare items normally (don't change types)

--- bin/notification/basket_event_stack.php
+++ bin/notification/basket_event_stack.php
@@ -85,7 +85,7 @@ foreach ($baskets as $basket) {
                 'data'      => [$diffusionParams]
             ]);
 
-            $users = array_filter(array_unique($usersOfEntities), function ($userOfEntities) use ($users){
+            $users = array_filter(array_unique($usersOfEntities, SORT_REGULAR), function ($userOfEntities) use ($users){
                 foreach ($users as $user) {
                     if ($user['id'] == $userOfEntities['id']) {
                         return $user;
